9e672715b5a0a362b2e9e394e725a922f908fe1f
singhkunal2050 <singhkunal2050@gmail.com>
2020-06-15 14:13:28 +0530

    containers and padding margin

--- lib/main.dart
+++ lib/main.dart
@@ -15,60 +15,76 @@ class NewApp extends StatelessWidget {
           backgroundColor: Colors.pink[700],
           centerTitle: true,
         ),
-        body: Center(
-            child: Column(
-          children: [
-            Text(
-              'Kunal Lorem19',
-              style: TextStyle(
-                backgroundColor: Colors.deepOrange,
-                fontSize: 28,
-                fontWeight: FontWeight.w800,
-                letterSpacing: 2,
-                color: Colors.deepPurple,
-                fontFamily: 'IndieFlower',
+        body: Container(
+          color: Colors.amber,
+          // margin: EdgeInsets.all(10),
+          margin: EdgeInsets.fromLTRB(5, 10, 15, 20),
+          padding: EdgeInsets.symmetric(
+            horizontal: 100,
+            vertical: 50,
+          ),
+          child: Center(
+              child: Column(
+            children: [
+              Text(
+                'Kunal Lorem19',
+                style: TextStyle(
+                  backgroundColor: Colors.deepOrange,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w800,
+                  letterSpacing: 2,
+                  color: Colors.deepPurple,
+                  fontFamily: 'IndieFlower',
+                ),
               ),
-            ),
-            // Text("Another one"),
-            Icon(
-              Icons.access_alarm,
-              color: Colors.red,
-              size: 80,
-            ),
-            RaisedButton(
-              onPressed: () => print("pressed"),
-              child: Text("Press"),
-              color: Colors.amberAccent,
-            ),
-            RaisedButton.icon(
-              onPressed: () => print("pressed 2"),
-              icon: Icon(
-                Icons.mail,
+              // Text("Another one"),
+              Icon(
+                Icons.access_alarm,
+                color: Colors.red,
+                size: 80,
               ),
-              label: Text("Send"),
-            ),
-            FlatButton(
-              onPressed: () => print("pressed 3"),
-              child: Icon(
-                Icons.add_a_photo,
+              RaisedButton(
+                onPressed: () => print("pressed"),
+                child: Text("Press"),
+                color: Colors.amberAccent,
               ),
-              color: Colors.blueAccent,
-            ),
-            // Icon(Icons.add_circle),
-            // Icon(Icons.add_location),
-            // Image(
-            //     image: NetworkImage(
-            //         'https://images.unsplash.com/photo-1589609966838-5c628d5c1aa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80')),
-            Image(
-              image: AssetImage('assets/champs.jpg'),
-              height: 156,
-            ),
-            // short hand for upper syntax
-            // Image.asset('assets/comp.jpg'),
-            // Image.network(
-            //     'https://images.unsplash.com/photo-1589609966838-5c628d5c1aa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'),
-          ],
-        )),
+              RaisedButton.icon(
+                onPressed: () => print("pressed 2"),
+                icon: Icon(
+                  Icons.mail,
+                ),
+                label: Text("Send"),
+              ),
+              FlatButton(
+                onPressed: () => print("pressed 3"),
+                child: Icon(
+                  Icons.add_a_photo,
+                ),
+                color: Colors.blueAccent,
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.add_a_photo,
+                  color: Colors.red,
+                ),
+                onPressed: () => print('YAYYYYYYYYYyy'),
+              ),
+              // Icon(Icons.add_circle),
+              // Icon(Icons.add_location),
+              // Image(
+              //     image: NetworkImage(
+              //         'https://images.unsplash.com/photo-1589609966838-5c628d5c1aa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80')),
+              Image(
+                image: AssetImage('assets/champs.jpg'),
+                height: 156,
+              ),
+              // short hand for upper syntax
+              // Image.asset('assets/comp.jpg'),
+              // Image.network(
+              //     'https://images.unsplash.com/photo-1589609966838-5c628d5c1aa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'),
+            ],
+          )),
+        ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.amber[700],
           onPressed: null,
@@ -85,5 +101,5 @@ class NewApp extends StatelessWidget {
 
 // for local edit .yaml file for assets
 // also work on flutter extentions lovely features to shorten the code
-// flatbutton has no shadow 
+// flatbutton has no shadow
 // raisedbutton has shadow
